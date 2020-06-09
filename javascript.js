c402da2c741d3831c376c366321ef8df528c0f73
roakoth <roseakoth2015@gmail.com>
2020-06-09 02:47:45 -0700

    added functionality

--- javascript.js
+++ javascript.js
@@ -1,133 +1,103 @@
-//Function to generate quotes
-let quote ="";
+/*
+ Object of Quotes with different fragments or parts of sentences. The print function
+ uses Math.random to get a random number to a max of the length in the array
+ quote then adds these fragments together and the value is returned
+ */
+let quote = "";
+
 const generateQuotes = {
-   init:function(fragment1,fragment2,fragment3,fragment4){
- this.fragment1 = fragment1;
- this.fragment2 = fragment2;
- this.fragment3 = fragment3;
- this.fragment4 = fragment4;
-   },
+  init: function (fragment1, fragment2, fragment3, fragment4, fragment5) {
+    this.fragment1 = fragment1;
+    this.fragment2 = fragment2;
+    this.fragment3 = fragment3;
+    this.fragment4 = fragment4;
+    this.fragment5 = fragment5;
+  },
 
-   print:function(){
-const fragment1 = Math.floor(Math.random()*(this.fragment1.length));
-const fragment2 = Math.floor(Math.random()*(this.fragment2.length));
-const fragment3 = Math.floor(Math.random()*(this.fragment3.length));
-const fragment4 = Math.floor(Math.random()*(this.fragment4.length));
+  print: function () {
+    const fragment1 = Math.floor(Math.random() * this.fragment1.length);
+    const fragment2 = Math.floor(Math.random() * this.fragment2.length);
+    const fragment3 = Math.floor(Math.random() * this.fragment3.length);
+    const fragment4 = Math.floor(Math.random() * this.fragment4.length);
+    const fragment5 = Math.floor(Math.random() * this.fragment5.length);
 
-quote = (this.fragment1[fragment1] + " " + this.fragment2[fragment2] + " " + this.fragment3[fragment3] + " " + this.fragment4[fragment4]);
+    quote =
+      this.fragment1[fragment1] +
+      " " +
+      this.fragment2[fragment2] +
+      " " +
+      this.fragment3[fragment3] +
+      this.fragment4[fragment4] +
+      " " +
+      this.fragment5[fragment5];
 
-return quote;
+    return quote;
+  },
+};
 
-   }
-}
+const success = Object.create(generateQuotes);
+success.init(
+  [
+    "For some people",
+    "Every success",
+    "My hope for the future",
+    "But hope",
+    "Even in the mud and scum of things",
+  ],
 
-//Success quotes
-let success = new Map();
-success.set(
- "begSuccess", ['For some people',
- 'Every success',
- 'My hope for the future',
- 'But hope',
- 'Even in the mud and scum of things']);
+  [
+    "their fear to lose is greater than the desire to succeed",
+    "whether it is the biggest or the smallest",
+    "not just in the music industry",
+    "like heroes",
+    "something always",
+  ],
+  [
+    "so they end up doing nothing and their dreams become impossible",
+    "inspires the joy and beauty of life",
+    "but in every young girl I meet is that they all realize their worth and ask for it",
+    "can prove hard to kill",
+    "always sings",
+  ],
+  [".", "!", ".", ".", "!"],
+  [
+    "Jordan Belfort",
+    "Gary Vaynerchuk",
+    "Dr Lucas D. Shallua",
+    "Jasleen Kour Gumbler",
+    "Compton Gage",
+  ]
+);
 
- success.set("middleSuccess",[
-    'their fear to lose is greater than the desire to succeed',
-    'whether it is the biggest or the smallest',
-    'not just in the music industry',
-    'like heroes',
-    'something always'    
- ]);
- success.set("endSuccess",[
-    'so they end up doing nothing and their dreams become impossible',
-    'inspires the joy and beauty of life',
-    'but in every young girl I meet is that they all realize their worth and ask for it',
-    'can prove hard to kill',
-    'always sings'    
- ]);
- success.set("authorsSuccess",[
-    'Jordan Belfort', 'Gary Vaynerchuk', 'Dr Lucas D. Shallua','Jasleen Kour Gumbler', 'Compton Gage' 
- ]);
-
- //Life quotes 
- let life = new Map();
- life.set(
-     "begLife", [
-     'Wisdom is knowing I am nothing',
-     'When in a relationship',
-     'Too many people spend money they have not earned',
-     'Sorrow looks back',
-     'Nolstalgia in reverse' 
- ]);
- life.set("middleLife",[
-    'love is knowing am everything',
-    'a real man does not make his woman jealous of others',
-    'to buy things they do not want',
-    'worry looks around',
-    'the longing for yet another strange land'
- ]);
- life.set("endLife",
- ['and between the two my lives moves',
- 'he makes others jealous of his woman',
- 'to impress people they do not like',
- 'faith looks up',
- 'grew especially strong in spring']);
- life.set("authorLife",
- [
-    'Nisargadatta Maharaj',
-    'Steve Maraboli',
-    'Will Rogers',
-    'Ralph Waldo Emerson',
-    'Vladimir Nabokov'   
- ]);
-
- //converting map success values into an array
-const successQuotes = [...success.values()]
-
-//converting map life values into an array
-const lifeQuotes = [...life.values()]
-console.log(lifeQuotes);
-
-// const quoteButton = document.getElementById('btn-lg');
-// const quote_display = document.getElementById('quoteDisplay');
-// const quote_dropdown = document.getElementById('quote-type');
-// let quote = '';
-// let quote_type = 1;
-// let quote_num = 1;
-
-//function to generate random number
-// function randomNumber(){
-//     var randomNum = Math.floor(Math.random()*5);
-//     return randomNum;
-// }
-
-//function to generate quotes
-// function randomNewQuote(){
-//     var begRandom = randomNumber();
-//     var middleRandom = randomNumber();
-//     var endRandom = randomNumber();
-//     var authorsRandom = randomNumber();
-
-// quote = begSuccess[begRandom] + ' ' + middleSuccess[middleRandom] + ' ' + endSuccess[endRandom]+ ' ' + authorsSuccess[authorsRandom];
-   
-    //quote = begLife[begRandom] + ' ' + middleLife[middleRandom] + ' ' + endLife[endRandom]+ ' ' + authorsLife[authorsRandom]; 
-    
- 
-//     return quote; 
-
-// }
-
-// quoteButton.addEventListener('click',()=>{
-   
-//     let CompleteQuote = randomNewQuote(quote_type);
-//     quote_display.innerHTML = CompleteQuote;   
-// });
-
-
-
-    
-    
-    
-
-    
-    
-
+const life = Object.create(generateQuotes);
+life.init(
+  [
+    "Wisdom is knowing I am nothing",
+    "When in a relationship",
+    "Too many people spend money they have not earned",
+    "Sorrow looks back",
+    "Nolstalgia in reverse",
+  ],
+  [
+    "love is knowing am everything",
+    "a real man does not make his woman jealous of others",
+    "to buy things they do not want",
+    "worry looks around",
+    "the longing for yet another strange land",
+  ],
+  [
+    "and between the two my lives moves",
+    "he makes others jealous of his woman",
+    "to impress people they do not like",
+    "faith looks up",
+    "grew especially strong in spring",
+  ],
+  [".", "!", ".", ".", "!"],
+  [
+    "Nisargadatta Maharaj",
+    "Steve Maraboli",
+    "Will Rogers",
+    "Ralph Waldo Emerson",
+    "Vladimir Nabokov",
+  ]
+);
--- quotegenerator.js
+++ quotegenerator.js
@@ -1,133 +1,100 @@
+let errorQuote = "Please choose the type of quotes";
+let errorNumber = "Please enter the number of quotes";
 
-let quote ="";
+const quoteButton = document.getElementById("btn-lg");
+const quote_display = document.getElementById("quoteDisplay");
+const radioButtons = document.getElementsByName("quote_types");
+const quote_num = document.getElementById("number_quotes");
+const errors = document.getElementById("error");
+const container = document.querySelector(".wrapper");
+const printMoreButton = document.createElement("button");
+const quit = document.createElement("button");
 
-const quoteButton = document.getElementById('btn-lg');
-const quote_display = document.getElementById('quoteDisplay');
-const radioButtons = document.getElementsByName('quote_types');
-const quote_num = document.getElementById('number_quotes');
-const errors = document.getElementById('error');
+printMoreButton.classList.add("button");
+quit.classList.add("button");
 
-const generateQuotes = {
-   init:function(fragment1,fragment2,fragment3,fragment4,fragment5){
- this.fragment1 = fragment1;
- this.fragment2 = fragment2;
- this.fragment3 = fragment3;
- this.fragment4 = fragment4;
- this.fragment5 = fragment5;
-   },
-
-   print:function(){
-const fragment1 = Math.floor(Math.random()*(this.fragment1.length));
-const fragment2 = Math.floor(Math.random()*(this.fragment2.length));
-const fragment3 = Math.floor(Math.random()*(this.fragment3.length));
-const fragment4 = Math.floor(Math.random()*(this.fragment4.length));
-const fragment5 = Math.floor(Math.random()*(this.fragment5.length));
-
-quote = (this.fragment1[fragment1] + " " + this.fragment2[fragment2] + " " + this.fragment3[fragment3] + this.fragment4[fragment4] + " " + this.fragment5[fragment5]);
-
-return quote;
-
-   }
-}
-
-const success = Object.create(generateQuotes);
-success.init(
-    [
-'For some people',
- 'Every success',
- 'My hope for the future',
- 'But hope',
- 'Even in the mud and scum of things'
-    ],
-
-    [
-        'their fear to lose is greater than the desire to succeed',
-        'whether it is the biggest or the smallest',
-        'not just in the music industry',
-        'like heroes',
-        'something always'  
-    ],
-    [
-        'so they end up doing nothing and their dreams become impossible',
-    'inspires the joy and beauty of life',
-    'but in every young girl I meet is that they all realize their worth and ask for it',
-    'can prove hard to kill',
-    'always sings'
-    ],
-    [
-        '.',
-        '!',
-        '.',
-        '.',
-        '!'
-    ],
-    [
-        'Jordan Belfort', 'Gary Vaynerchuk', 'Dr Lucas D. Shallua','Jasleen Kour Gumbler', 'Compton Gage' 
-    ]
-);
-
-
-const life = Object.create(generateQuotes);
-life.init([
-    'Wisdom is knowing I am nothing',
-     'When in a relationship',
-     'Too many people spend money they have not earned',
-     'Sorrow looks back',
-     'Nolstalgia in reverse'
-],
-[
-    'love is knowing am everything',
-    'a real man does not make his woman jealous of others',
-    'to buy things they do not want',
-    'worry looks around',
-    'the longing for yet another strange land'
-],
-[
-    'and between the two my lives moves',
- 'he makes others jealous of his woman',
- 'to impress people they do not like',
- 'faith looks up',
- 'grew especially strong in spring'
-],
-[
-    '.',
-    '!',
-    '.',
-    '.',
-    '!'
-],
-[
- 'Nisargadatta Maharaj',
- 'Steve Maraboli',
- 'Will Rogers',
- 'Ralph Waldo Emerson',
- 'Vladimir Nabokov'   
-     ]);
-
-     //function to generate quotes depending on the type of quotes
-function displayQuotes(){
-    
-    for(let i=0; i<radioButtons.length; i++){
-        if(radioButtons[i].checked && quote_num.value === "1"){
-            if(radioButtons[i].value === 'successValue'){
-                quote_display.innerHTML = success.print();  
-            }else{
-                quote_display.innerHTML = life.print();
-            }
-            
-        }
+/*
+ Function to get the value of the radio button that is checked and return it
+ */
+function getRadioResult() {
+  for (let i = 0; i < radioButtons.length; i++) {
+    if (radioButtons[i].checked) {
+      return radioButtons[i].value;
     }
+  }
+}
+/*
+ Function to display the quote depending on the value of radio button that is checked
+ */
+function displayQuotes(radioResult) {
+  if (radioResult === "successValue") {
+    quote_display.innerHTML += success.print() + "<br>";
+  } else {
+    quote_display.innerHTML += life.print() + "<br>";
+  }
 }
 
-//generate more quotes depending on number of quotes selected
+/*
+ Select the number of quotes to be printed and print messages if errors
+ Run a for loop for the number the user has input and run the displayQuotes function that multiple times
+ */
 
-//button to generate quotes
- quoteButton.addEventListener('click',()=>{
+function generateMoreQuotes(radioResult) {
+  let num_quotes = quote_num.value;
+  errors.className = "error";
+  if (!radioResult) {
+    errors.innerHTML = errorQuote;
+  } else if (!num_quotes) {
+    errors.innerHTML = errorNumber;
+  } else {
+    errors.innerHTML = "";
+    for (let i = 0; i < quote_num.value; i++) {
+      displayQuotes(radioResult);
+    }
 
-displayQuotes();
+    quote_num.value = "";
+    container.appendChild(printMoreButton);
+    container.appendChild(quit);
+    quit.innerHTML = "Quit";
+    printMoreButton.innerHTML = "Print more quotes";
+    quoteButton.remove();
+  }
+}
 
-});  
- 
- 
+/*
+printMoreButton resets everything making the user to input number of quotes and generate 
+more quotes
+*/
+printMoreButton.addEventListener("click", () => {
+  quote_display.innerHTML = "";
+  quoteButton.innerHTML = "New quote";
+  container.appendChild(quoteButton);
+  quit.remove();
+  printMoreButton.remove();
+});
 
+/*
+quit is a button that calls the function closeWindow that prompts the user if he wants to close the window 
+or to continue generating more quotes
+*/
+quit.addEventListener("click", () => {
+  closeWindow();
+});
 
+//closing the window or continue generating quotes
+function closeWindow() {
+  if (confirm("Do you really want to close the window and exit the program?")) {
+    close();
+  }
+}
+
+/*
+ event listener on click that gets the value of the radio button and stores it in a variable
+ this is then passed to the generateMoreQuotes function so that we know which quote to print
+ as this value is then passed down to the displayQuotes function
+ */
+quoteButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  let radioResult = getRadioResult();
+  generateMoreQuotes(radioResult);
+});
